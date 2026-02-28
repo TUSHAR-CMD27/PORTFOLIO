82b846c9215e70827d6c34a311312a1f1a5b8d9f
Tushar Gadhari <tushargadhari3@gmail.com>
2026-02-28 14:31:42 +0530

    skills update 2

--- Frontend/portf/src/pages/Skills.jsx
+++ Frontend/portf/src/pages/Skills.jsx
@@ -5,15 +5,15 @@ import FlowingMenu from '../components/FlowingMenu';
 
 const Skills = () => {
   const skillItems = [
-    { link: '#', text: 'JavaScript', image: '' },
-    { link: '#', text: 'CSS', image: '' },
-    { link: '#', text: 'HTML', image: '' },
-    { link: '#', text: 'Python', image: '' },
-    { link: '#', text: 'Java', image: '' },
-    { link: '#', text: 'MongoDB', image: '' },
-    { link: '#', text: 'Node.js', image: '' },
-    { link: '#', text: 'Express', image: '' },
-    { link: '#', text: 'Machine Learning', image: '' },
+    { link: '#', text: 'JavaScript', image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' },
+    { link: '#', text: 'CSS', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg' },
+    { link: '#', text: 'HTML', image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg' },
+    { link: '#', text: 'Python', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg' },
+    { link: '#', text: 'Java', image: 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg' },
+    { link: '#', text: 'MongoDB', image: 'https://upload.wikimedia.org/wikipedia/en/4/45/MongoDB-Logo.svg' },
+    { link: '#', text: 'Node.js', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg' },
+    { link: '#', text: 'Express', image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png' },
+    { link: '#', text: 'Machine Learning', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/256px-Scikit_learn_logo_small.svg.png' },
   ];
 
   return (
